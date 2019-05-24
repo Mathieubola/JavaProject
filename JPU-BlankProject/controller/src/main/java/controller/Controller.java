5e787b81c3c25cf12a957de8335acb372e9ab78c
Mathieu <mathieulassurguere@hotmail.fr>
2019-05-24 13:35:54 +0200

    Update Controller

--- JPU-BlankProject/controller/src/main/java/controller/Controller.java
+++ JPU-BlankProject/controller/src/main/java/controller/Controller.java
@@ -1,94 +1,72 @@
 package controller;
 
 import contract.ControllerOrder;
-import contract.IController;
 import contract.IModel;
 import contract.IView;
+import entity.Entity;
+import entity.Player;
 
-/**
- * The Class Controller.
- */
-public final class Controller implements IController {
+public class Controller {
+	
+	private IView view;
+	private IModel model;
+	
+	private int width = 50; //Hauteur du terrain
+	private int height = 50; //Largeur du terrain
+	private int score = 0; //Score du joueur
+	private int delay = 30; //Delay entre les frames (30 fps -> 1000/30 = 29.333... ~= 30 ms)
+	
+	private Entity[][] entitys = new Entity[width][height]; //Tableau fixe contenant toute les entité du tableau (les rocher, diamant et tt)
+	private Player player;
+	
+	private ControllerOrder direction;
 
-	/** The view. */
-	private IView		view;
-
-	/** The model. */
-	private IModel	model;
-
-	/**
-	 * Instantiates a new controller.
-	 *
-	 * @param view
-	 *          the view
-	 * @param model
-	 *          the model
-	 */
 	public Controller(final IView view, final IModel model) {
 		this.setView(view);
 		this.setModel(model);
 	}
-
-	/**
-     * Control.
-     */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IController#control()
-	 */
+	
 	public void control() {
-		this.view.printMessage("Appuyer sur les touches 'E', 'F', 'D' ou 'I', pour afficher Hello world dans la langue d votre choix.");
+		
 	}
+	
+	public void start() {
+		while (player.isAlive()) {
+			this.movePlayer();
+			this.moveFallingObject();
+			this.collision();
+			this.updateAnimation();
+		}
+	}
+	
+	public void movePlayer() {
+		//En fonction de la direction
+	}
+	
+	public void moveFallingObject() {
+		//Faire tomber les diams et les pierre
+	}
+	
+	public void collision() {
+		//Checker si on c'est pas pris un rochet sur la geul ou si on est pas sur un crystal ou un portail
+	}
+	
+	public void updateAnimation() {
+		
+	}
+	
+	
 
-	/**
-     * Sets the view.
-     *
-     * @param pview
-     *            the new view
-     */
 	private void setView(final IView pview) {
 		this.view = pview;
 	}
 
-	/**
-	 * Sets the model.
-	 *
-	 * @param model
-	 *          the new model
-	 */
 	private void setModel(final IModel model) {
 		this.model = model;
 	}
 
-	/**
-     * Order perform.
-     *
-     * @param controllerOrder
-     *            the controller order
-     */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IController#orderPerform(contract.ControllerOrder)
-	 */
 	public void orderPerform(final ControllerOrder controllerOrder) {
-		switch (controllerOrder) {
-			case English:
-				this.model.loadHelloWorld("GB");
-				break;
-			case Francais:
-				this.model.loadHelloWorld("FR");
-				break;
-			case Deutsch:
-				this.model.loadHelloWorld("DE");
-				break;
-			case Indonesia:
-				this.model.loadHelloWorld("ID");
-				break;
-			default:
-				break;
-		}
+		
 	}
-
+	
 }
