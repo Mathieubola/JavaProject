dcedd2780f31a0a97c23050a6b85c111fce488df
TristanDplr <tristan.dplr@gmail.com>
2019-06-03 16:48:50 +0200

    Adding the ModelTest

---
+++ JPU-BlankProject/model/modelTest/model/ModelTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ModelTest {
+	private Model model;
+	private int nbMap = 1;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		this.model = new Model();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetMap() {
+		assertNotNull(model.getMap(nbMap));
+	}
+
+}
