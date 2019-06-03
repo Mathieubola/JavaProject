52f6b484ea589beb9ad0a40d4298df025db9c3fa
Donovan <donovan.hoang@viacesi.fr>
2019-06-03 19:51:35 +0200

    Adding javadic for mapTest

--- JPU-BlankProject/entity/entityTest/entity/MapTest.java
+++ JPU-BlankProject/entity/entityTest/entity/MapTest.java
@@ -6,20 +6,40 @@ import org.junit.*;
 
 import entity.Map;
 
+/**
+ * The mapTest class
+ * 
+ */
 public class MapTest {
 
+	/**
+	 * Declaration of a map
+	 */
 	public static Map map;
-
+	
+	/**
+	 * Instanciation of the map
+	 * The has empty name ("") and his dimensions are 0 and 0
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		map = new Map("", 0, 0);
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 	
 
+	/**
+	 * The test class for the getName method
+	 * "" is the expected name of the map and this method compare it with the result of the getName method
+	 */
 	@Test
 	public void testGetName() {
 		  final String expected = "";
@@ -27,6 +47,10 @@ public class MapTest {
 	        System.out.println("Name is a success");
 	    }
 
+	/**
+	 * The test class for the getHeight method
+	 * 0 is the expected height of the map and this method compare it with the result of the getHeight method
+	 */
 	@Test
 	public void testGetHeight() {
 		  final int expected = 0;
@@ -34,6 +58,10 @@ public class MapTest {
 	        System.out.println("Height is a success");
 	    }
 
+	/**
+	 * The test class for the getWidth method
+	 * 0 is the expected width of the map and this method compare it with the result of the getWidth method
+	 */
 	@Test
 	public void testGetWidth() {
 		  final int expected = 0;
